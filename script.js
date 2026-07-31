fda808be842c63f70680047dbf3cc3819fa1a870
rafay5451 <rafay2005451@gmail.com>
2026-07-31 18:20:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,23 +9,7 @@ const images = [
 "images/IMG-20250125-WA0028.jpg",
 "images/IMG-20250531-WA0023.jpg",
 "images/IMG-20250531-WA0040.jpg",
-"images/IMG-20250531-WA0044.jpg",
-"images/IMG-20250618-WA0020.jpg",
-"images/IMG-20250702-WA0034.jpg",
-"images/IMG-20250702-WA0036.jpg",
-"images/IMG-20250712-WA0057.jpg",
-"images/IMG-20251025-WA0030.jpg",
-"images/IMG-20251207-WA0003.jpg",
-"images/IMG-20251207-WA0009.jpg",
-"images/IMG-20260215-WA0046.jpg",
-"images/IMG-20260215-WA0047.jpg",
-"images/IMG-20260419-WA0004.jpg",
-"images/IMG-20260427-WA0045.jpg",
-"images/IMG20251201163853_01.jpg",
-"images/IMG20251201163853_03.jpg",
-"images/Snapchat-775781863.jpg",
-"images/Snapchat-1814954628.jpg",
-"images/Snapchat-2139462222.jpg"
+
 ];
 
 let current = 0;
